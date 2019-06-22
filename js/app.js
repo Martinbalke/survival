352602db6e35df609670fa5fcd8d3e352f25dd63
Valerie Sobol <vssobol@gmail.com>
2019-06-22 11:56:31 -0700

    js changes

--- js/app.js
+++ js/app.js
@@ -1,14 +1,76 @@
 'use strict';
 
-// badPoints = 1
-// neutralPoints = .5 (badPoints)
-// goodPoints = 1
-
-var badPoints;
-var goodPoints;
-
 //function for generating questions
 	//affect user score
 	//generate three questions and assign them value
 //function for rendering questions to the dom
 //function for rendering the ending page
+
+function generateStoryNode(storyNode){
+    var sectionEl = document.getElementById('story');
+    var pEl = document.createElement('p');
+    var storyNode = storyNode;
+    pEl.innerHTML = storyNode;
+    sectionEl.appendChild(pEl);
+}
+
+function generateQuestion(question){
+    var sectionEl = document.getElementById('questionPrompt');
+    var pEl = document.createElement('p');
+    var question = question;
+    pEl.innerHTML = question;
+    sectionEl.appendChild(pEl);
+}
+
+var badPoints;
+var goodPoints;
+
+function generateDecision(decision, morality){
+    var sectionEl = document.getElementById('decisions');
+    var pEl = document.createElement('p');
+    var decision = decision;
+    pEl.innerHTML = decision;
+    var morality = morality;
+
+    if(morality === 'good'){
+        pEl.setAttribute('id', 'question1');
+        goodPoints++;
+    } else if(morality === 'bad'){
+        pEl.setAttribute('id', 'question2');
+        badPoints++;
+    } else if(morality === 'neutral'){
+        pEl.setAttribute('id', 'question3');
+        badPoints += .5;
+    } else{
+        console.log("invalid morality");
+    }
+    sectionEl.appendChild(pEl);
+}
+
+function randomizer(min, max){
+    return Math.floor(Math.random() * (max - min + 1) * min);
+}
+
+function story(){
+
+    generateStoryNode(
+        "lorem ipsum"
+    );
+
+    for(var i = 0; i < 3; i++){
+        generateQuestion(
+            "lorem ipsum"
+        , 'bad');
+
+        generateDecision(
+            "lorem ipsum"
+        , 'neutral');
+
+        generateDecision(
+            "lorem ipsum"
+        , 'good');
+    }
+
+}
+
+story();
