19f49be47342cd74ba920049c5684c08dd6d6455
Valerie Sobol <vssobol@gmail.com>
2019-06-27 19:14:32 -0700

    js update

--- js/data.js
+++ js/data.js
@@ -1,7 +1,9 @@
 'use strict';
 
-var buttons = [];
+var buttons = [node3G7, node3N7, node3B7, node3G12, node3N12, node3B12, node3B4, node2B1, node2G3, node3G6, node3N4, node2N1, node2N3, node3N6, node3G4, node2G1, node1B0, node1N0, node1G0, node2B3, node3B6, node3B8, node3N8, node3G8, node3B11, node3N11, node3G11, node3G9, node2B2, node2N2, node2G2, node3B10, node3N9, node3N10, node3B5, node3N5, node3G5, node3B9, node3G10];
 
-for(var i = 0; i < 39; i++){
 
+
+for(var i = 0; i < buttons.length; i++){
+    var button = document.getElementById([i]);
 }
