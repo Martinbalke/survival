627d7c35dc1841373f94740b064ee40115a059ed
Martin Balke <Mrtnbalke@gmail.com>
2019-06-27 18:33:43 -0700

    end screen and start screen

--- js/ending.js
+++ js/ending.js
@@ -1,7 +1,8 @@
 'use strict'
 var divEL_endingText = document.getElementById('endingText');
 let story = localStorage.getItem('story');
-
+let team = localStorage.getItem('team');
+team = JSON.parse(team);
 function endingChoices(){
   
   let pEl = document.createElement('p');
